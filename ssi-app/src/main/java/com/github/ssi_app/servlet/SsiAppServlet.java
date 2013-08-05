01984931adc4b732eda91900565602a47ff5aa2c
Eric R Glass <ericrglass@gmail.com>
2013-08-05 19:04:20 -0400

    Initial revision for the ssi-app GitHub repository.

---
+++ ssi-app/src/main/java/com/github/ssi_app/servlet/SsiAppServlet.java
@@ -0,0 +1,9 @@
+package com.github.ssi_app.servlet;
+
+import com.github.ssi_servlet.SsiServlet;
+
+public class SsiAppServlet extends SsiServlet {
+
+	private static final long serialVersionUID = -5364812322312373560L;
+
+}
