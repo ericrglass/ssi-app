c8fa28b4d1cf264b7f1b83858d4ea0a9914b9609
Eric R Glass <ericrglass@gmail.com>
2013-09-17 18:46:50 -0400

    Added logger for Java.

---
+++ ssi-app/src/main/java/com/github/ssi_app/SsiAppLogger.java
@@ -0,0 +1,9 @@
+package com.github.ssi_app;
+
+import java.util.logging.Logger;
+
+public interface SsiAppLogger {
+
+	public static final Logger LOGGER = Logger.getLogger("com.github.ssi_app");
+
+}
